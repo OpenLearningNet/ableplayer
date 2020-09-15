ce6eea556f7343a310466b845e47c24b4091f424
Vince Tripodi <vincetripodi@MacBook-Pro.local>
2020-09-15 15:31:01 +1000

    New type defs for methods

--- build/ableplayer.d.ts
+++ build/ableplayer.d.ts
@@ -1,4 +1,11 @@
 declare module 'ableplayer' {
+	type AblePlayerState =
+	| 'stopped' // - Not yet played for the first time, or otherwise reset to unplayed.
+	| 'ended' // - Finished playing.
+	| 'paused' // - Not playing, but not stopped or ended.
+	| 'buffering' // - Momentarily paused to load, but will resume once data is loaded.
+	| 'playing'; // - Currently playing.
+	
 	interface AblePlayerOptions {
 		onLoaded?: () => void;
 		onPlay?: () => void;
@@ -7,5 +14,7 @@ declare module 'ableplayer' {
 	export class AblePlayer{
 		constructor(media: HTMLVideoElement | null, options: AblePlayerOptions);
 		getDuration(): Promise<number>;
+		getPlayerState(): Promise<AblePlayerState>;
+		getElapsed(): Promise<number>;
 	}
 }
