3f14ea034272003b646d653050ff3112aa60d07c
Kelvin Tse <post@ktml.net>
2025-01-22 21:45:51 +1100

    Test

--- scripts/buildplayer.js
+++ scripts/buildplayer.js
@@ -1110,6 +1110,7 @@ var Cookies = require("js-cookie");
 						$pipe.addClass('icon-pipe');
 					}
 					else {
+						console.log("!!!!!")
 						var pipeIcon = this.getIcon('pipe');
 						$pipeImg = $('<img>', {
 							src: pipeIcon,
@@ -1126,7 +1127,7 @@ var Cookies = require("js-cookie");
 						buttonImgSrc = this.getIcon(this.volumeButton);
 					}
 					else if (control === 'fullscreen') {
-						buttonImgSrc = this.getIcon('pipe');
+						buttonImgSrc = this.getIcon('fullscreen-expand');
 					}
 					else if (control === 'slower') {
 						if (this.speedIcons === 'animals') {
